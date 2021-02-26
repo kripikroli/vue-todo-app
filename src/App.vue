6bbcbdd3a7612e3bbe08c1da448ea93ce584fc2b
A-aron Paul Luminding <aaron@cyfusions.com>
2021-02-25 23:42:11 -0500

    Allow user to create and delete todos.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: false
+      }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,46 @@
+<template>
+    <form @submit="addTodo">
+        <input type="text" v-model="title" name="title" placeholder="Add todo...">
+        <input type="submit" value="Submit" class="btn">
+    </form>
+</template>
+
+<script>
+import uuid from 'uuid';
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(e) {
+            e.preventDefault();
+            const newTodo = {
+                id: uuid.v4(),
+                title: this.title,
+                completed: false
+            }
+            this.$emit('add-todo', newTodo);
+
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+
+    input[type="submit"] {
+        flex: 2;
+    }
+</style>
