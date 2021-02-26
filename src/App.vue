18c2e9c40422532b6dc637d3997495d25efe6907
A-aron Paul Luminding <aaron@cyfusions.com>
2021-02-26 00:48:59 -0500

    Added data from fake REST API.

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import axios from 'axios';
 export default {
   name: 'App',
   components: {
@@ -19,32 +20,33 @@ export default {
   },
   data() {
     return {
-      todos: [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      }
-      ]
+      todos: []
     }
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+        .catch(err => console.log(err));
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const {title, completed} = newTodo;
+      //this.todos = [...this.todos, newTodo];
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err));
     }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
   }
 }
 </script>
--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -6,7 +6,6 @@
 </template>
 
 <script>
-import uuid from 'uuid';
 export default {
     name: "AddTodo",
     data() {
@@ -18,7 +17,6 @@ export default {
         addTodo(e) {
             e.preventDefault();
             const newTodo = {
-                id: uuid.v4(),
                 title: this.title,
                 completed: false
             }
